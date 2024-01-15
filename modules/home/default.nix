9eafd9123e210b748ddc08a345bbd5ad5241cfa8
Antiknight <leo@jmohr.net>
2024-01-15 19:24:03 +0000

    first draft

---
+++ modules/home/default.nix
@@ -0,0 +1,27 @@
+{inputs, username, ...}: {
+  imports =
+       [(import ./waybar)]
+    ++ [(import ./firefox)]
+#    ++ [(import ./vscodium)]
+    ++ [(import ./hyprland)]
+#    ++ [(import ./gaming)]
+    ++ [(import ./git)]
+    ++ [(import ./btop)]
+    ++ [(import ./bat)]
+    ++ [(import ./cava)]
+    ++ [(import ./kitty)]
+    ++ [(import ./micro)]            # nano replacement
+#    ++ [(import ./audacious)]       # music player
+#    ++ [(import ./unity)]
+#    ++ [(import ./rider)]           # C# JetBrain editor
+    ++ [(import ./discord)]         # discord with catppuccin theme
+    ++ [(import ./swaylock)]
+    ++ [(import ./wofi)]
+    ++ [(import ./zsh)]
+    ++ [(import ./gtk)]
+    ++ [(import ./mako)]            # notification deamon
+    ++ [(import ./scripts)]         # personal scripts
+    ++ [(import ./starship)]
+    ++ [(import ./nvim)]
+    ++ [(import ./package)];
+}
---
+++ modules/home/package/python.nix
@@ -0,0 +1,18 @@
+{pkgs, ...}: 
+{
+  environment.systemPackages = with pkgs; [
+    (python311Full.withPackages(ps: with ps; [ 
+      requests
+      pygame
+      shapely
+      
+      # github-email-explorer
+      # jinja2
+      # markupsafe
+      # python-http-client
+      requests
+      # sendgrid
+      # tabulate
+    ]))
+  ];
+}
