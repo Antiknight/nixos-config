ef121d2e7e98b27655c0ccb2ab1448495123213f
Antiknight <leo@jmohr.net>
2024-01-25 19:28:30 +0000

    starting work on neovim

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -1,6 +1,17 @@
 { inputs, pkgs, ... }:
 {
   programs.nixvim = {
-  	enable = true;
+    enable = true;
+	  colorschemes.tokyonight.enable = true;
+    colorschemes.tokyonight.transparent = true;
+  
+    plugins.telescope.enable = true;
+	  plugins.treesitter.enable = true;
+	  plugins.which-key.enable = true;
+
+	  options = {
+	    relativenumber = true;
+	    tabstop = 2;
+    };
   };
 }
