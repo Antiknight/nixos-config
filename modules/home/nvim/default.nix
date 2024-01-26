c077ebbec2d73b1247a559904bfe6453c15de259
Antiknight <leo@jmohr.net>
2024-01-26 21:10:00 +0000

    some more nvim

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -9,11 +9,29 @@
     plugins.telescope.enable = true;
 	  plugins.treesitter = {
       enable = true;
+      ensureInstalled = [
+        "rust"
+        "nix"
+        "lua"
+        "python"
+        ];
+      nixGrammars = true;
+      nixvimInjections = true;
     };
+    plugins.treesitter-context = {
+      enable = true;
+      maxLines = 3;
+    };
+    plugins.neo-tree = {
+      enable = true;
+      enableGitStatus = true;
+    };
+
 	  plugins.which-key.enable = true;
 		plugins.lsp.enable = true;
     
 	  options = {
+      number = true;
 	    relativenumber = true;
 	    tabstop = 2; 
 			shiftwidth = 2;
@@ -23,9 +41,26 @@
 
 		globals.mapleader = " ";
     keymaps = [
+    {
+      key = "<leader>e";
+      mode = ["n" "v"];
+      action = "<cmd>Neotree toggle<cr>";
+      options = {
+        desc = "Neotree toggle";
+      };
+    }
 		{
 		  key = "<leader>ff";
 			action = "<cmd>Telescope find_files<CR>";
+    }
+    {
+      key = "<leader>fw";
+      mode = ["n" "v"];
+      action = ''function() require("telescope.builtin").live_grep() end'';
+      lua = true;
+      options = {
+        desc = "Find Words";
+      };
     }
 		];
   };
